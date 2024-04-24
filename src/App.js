28f9e52ebe91ef4d77bcf63feeae4cabf72cb062
farsanau <mail2saana@gmail.com>
2024-04-24 12:55:49 +0300

    yaani initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Section1 from "./components/section1";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div>
+     <Section1/>
+     <Services/>
+     <Contact/>
+  <Footer/>
+       
+
+    
+    </div>
+  );
+}
+
+export default App;
